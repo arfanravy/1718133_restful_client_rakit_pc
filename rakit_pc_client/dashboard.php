d6c305363e0e573c05dd94dfc01ba21fe0d72439
arfanravy <47246369+arfanravy@users.noreply.github.com>
2020-12-31 18:34:32 +0700

    rest_client

---
+++ rakit_pc_client/dashboard.php
@@ -0,0 +1,5 @@
+<!-- Page Heading -->
+<h1 class="h3 mb-4 text-gray-800 text-center text-primary"><strong>RAKIT PC</strong></h1>
+
+<script src="https://unpkg.com/@lottiefiles/lottie-player@latest/dist/lottie-player.js"></script>
+<lottie-player class="mx-auto" src="https://assets3.lottiefiles.com/packages/lf20_xq4j1R.json"  background="transparent"  speed="1"  style="width: 500px; height: 500px;"  loop autoplay></lottie-player>
